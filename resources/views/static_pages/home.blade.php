c7fb376b0a50b3658b1d406e47934e0cb503051b
Jason Bourne <1508366362@qq.com>
2019-12-01 06:01:42 +0000

    用户登录

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,6 +4,6 @@
     <h1>Hello Miracle</h1>
     <p class="lead">你现在看到的是 <a href="#">Miracle's Weibo</a></p>
     <p>Everything begins from now in here</p>
-    <p><a class="btn btn-lg btn-success" role="button" href="#">Sign up</a></p>
+    <p><a class="btn btn-lg btn-success" role="button" href="{{ route('signup') }}" role="button">Sign up</a></p>
   </div>
 @stop
