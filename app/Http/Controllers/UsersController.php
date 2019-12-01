828b711f32234398148a16715fea1775c1f949d3
Jason Bourne <1508366362@qq.com>
2019-12-01 12:18:53 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,12 +10,12 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except'    =>  ['show', 'create', 'store']
+            'except'    =>  ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
             'only'  =>  ['create']
-        ])
+        ]);
     }
     public function create()
     {
@@ -69,6 +69,12 @@ class UsersController extends Controller
         session()->flash()->route('users.show', $user);
 
         return redirect()->route('users.show', $user->id);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 }
 
