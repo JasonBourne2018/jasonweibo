fe7530e36634480f3068972bd2b9bb3dd86d8cd9
JasonBounre <1508366362@qq.com>
2019-12-01 13:35:45 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Weibo App') - Miracle Weibo</title>
-  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css')}}">
-</head>
-<body>
+  <head>
+    <title>@yield('title', 'Weibo App') - Miracle Weibo</title>
+      <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css')}}">
+    </head>
+    <body>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">Weibo App</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-        <li class="nav-item"><a class="nav-link" href="/about">登录</a></li>
-      </ul>
-    </div>
-  </nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+              <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+              <li class="nav-item"><a class="nav-link" href="/about">登录</a></li>
+            </ul>
+        </div>
+      </nav>
 
-  <div class="container">
-    @yield('content')
-  </div>
-</body>
+
+      <div class="container">
+        @yield('content')
+      </div>
+    </body>
 </html>
