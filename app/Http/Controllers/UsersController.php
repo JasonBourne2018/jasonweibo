b8b22f71523aa129486d4219ba15f0ac0f0b5ca1
Jason Bourne <1508366362@qq.com>
2019-12-01 08:15:20 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,5 +16,15 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    public funtion store(Request $request)
+    {
+        $this->validate($request, [
+            'name'  =>  'required|max:50',
+            'email' =>  'required|email|unique:users|max:255',
+            'passwrod'  =>  'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
 
