8365045b6c0656dc18d0b566b30326bad9fb9e28
Jason Bourne <1508366362@qq.com>
2019-12-01 12:51:02 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,20 +5,15 @@ use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
         $users = factory(User::class)->times(50)->make();
-        User::insert($users->makeVisible(['password','remember_token'])->toArray());
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
         $user->name = 'Miracle-';
         $user->email = '1508366362@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
