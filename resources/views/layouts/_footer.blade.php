6f747c411a0d49e4a07a0e45cf9e19637b6b77be
JasonBounre <1508366362@qq.com>
2019-12-01 13:49:30 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="footer">
   <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-  <a href="/" target="_blank">Your Miracle.</a>
+  <a href="{{ route('home') }}" target="_blank">Your Miracle.</a>
 
   <div class="float-right">
-    <a href="/about">关于</a>
+    <a href="{{ route('help') }}">关于</a>
   </div>
 </footer>
