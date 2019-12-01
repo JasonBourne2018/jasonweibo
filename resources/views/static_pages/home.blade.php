ac58bb57e1523449fc15e7c6adeb1f37ff360101
Jason Bourne <1508366362@qq.com>
2019-12-01 16:20:24 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,9 +1,24 @@
 @extends('layouts.default')
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Miracle</h1>
-    <p class="lead">你现在看到的是 <a href="#">Miracle's Weibo</a></p>
-    <p>Everything begins from now in here</p>
-    <p><a class="btn btn-lg btn-success" role="button" href="{{ route('signup') }}" role="button">Sign up</a></p>
-  </div>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>Hello Miracle</h1>
+      <p class="lead">你现在看到的是 <a href="#">Miracle's Weibo</a></p>
+      <p>Everything begins from now in here</p>
+      <p><a class="btn btn-lg btn-success" role="button" href="{{ route('signup') }}" role="button">Sign up</a></p>
+    </div>
+  @endif
 @stop
