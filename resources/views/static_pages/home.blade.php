dcae589283b1f4397a2dff4d3604ee67d7966690
JasonBounre <1508366362@qq.com>
2019-12-01 12:56:38 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Miracle</h1>
+    <p class="lead">你现在看到的是 <a href="#">Miracle's Weibo</a></p>
+    <p>Everything begins from now in here</p>
+    <p><a class="btn btn-lg btn-success" role="button" href="#">Sign up</a></p>
+  </div>
 @stop
